3c40d655736476198e2ceeb19eeb82f403557883
Degradant <90028593+KotovichRoman@users.noreply.github.com>
2022-06-17 16:26:49 +0300

    fix task1

--- task1/task1.rb
+++ task1/task1.rb
@@ -1,105 +1,121 @@
-require "rubygems"
-require "csv"
-require "nokogiri"
-require "curl"
+# frozen_string_literal: true
+
+require 'csv'
+require 'nokogiri'
 require 'open-uri'
 
-url = "https://www.petsonic.com/farmacia-para-gatos/"
-file_name = "file.csv"
+url = 'https://www.petsonic.com/farmacia-para-gatos/'
+file_name = 'file.csv'
 
-def parse (url, file_name)
-    html = get_html(url)
+def parse(url, file_name)
+  html = get_html(url)
 
-    create_csv(file_name)
+  create_csv(file_name)
 
-    all_products = html.xpath('//input[@name = "n"]/@value').text.to_i
-    products_per_page = html.xpath('//a[@class = "product-name"]').size;
-    all_pages = (all_products / products_per_page.to_f).ceil
+  all_products = html.xpath('//input[@name = "n"]/@value').text.to_i
+  products_per_page = html.xpath('//a[@class = "product-name"]').size
+  all_pages = (all_products / products_per_page.to_f).ceil
 
-    (1..all_pages).each do |page_num|
-        html = get_html(url + "?p=" + "#{page_num}");
-        parse_page(html, file_name, products_per_page, all_products)
+  (1..all_pages).each do |page_num|
+    html = get_html(get_page_url(url, page_num))
 
-        all_products -= products_per_page
-    end
+    parse_page(html, file_name, products_per_page, all_products)
+
+    all_products -= products_per_page
+  end
 end
 
 def get_html(url)
-    http = URI.open(url)
-    Nokogiri::HTML(http)
+  http = URI.open(url)
+  Nokogiri::HTML(http)
+end
+
+def get_page_url(url, page_num)
+  "#{url}?p=#{page_num}"
 end
 
 def parse_page(html, file_name, products_per_page, all_products)
-    if all_products >= 25
-        (0...products_per_page).each do |product_num|
-            parse_product(html, file_nmae, product_num)
-        end
-    else
-        (0...all_products).each do |product_num|
-            parse_product(html, file_nmae, product_num)
-        end
+  if all_products >= 25
+    (0...products_per_page).each do |product_num|
+      parse_product(html, file_name, product_num)
     end
+  else
+    (0...all_products).each do |product_num|
+      parse_product(html, file_name, product_num)
+    end
+  end
 end
 
 def parse_product(html, file_name, product_num)
-    page_url = html.xpath('//div[@class = "product-desc display_sd"]//@href')[product_num]
-    page_html = get_html(page_url)
+  page_url = html.xpath('//div[@class = "product-desc display_sd"]//@href')[product_num]
 
-    product = get_info(page_html)
+  page_html = get_html(page_url)
 
-    show_data(product, file_name)
+  product = get_info(page_html)
+
+  show_data(product, file_name)
 end
 
 def get_info(html)
-    product_name = html.xpath('//h1[@class = "product_main_name"]').text.strip
-    product_img = html.xpath('//img[@id = "bigpic"]/@src')
-    product_weight_variation = html.xpath('//span[@class = "radio_label"]')
-    price_per_weight = html.xpath('//span[@class = "price_comb"]')
+  product_name = html.xpath('//h1[@class = "product_main_name"]').text.strip
+  product_img = html.xpath('//img[@id = "bigpic"]/@src')
+  product_weight_variation = html.xpath('//span[@class = "radio_label"]')
+  price_per_weight = html.xpath('//span[@class = "price_comb"]')
 
-    weights = Array.new
-    prices = Array.new
+  weights = transform_weight(product_weight_variation)
+  prices = transform_price(price_per_weight)
 
-    product_weight_variation.each_with_index do |weight, index|
-        weights[index] = weight.text.to_s
-    end
-
-    price_per_weight.each_with_index do |price, index|
-        prices[index] = price.text.to_s
-    end
-    
-    return [product_name, product_img, weights, prices]
+  [product_name, product_img, weights, prices]
 end
 
-def show_data (product, file_name)
-    (0...product[2].size).each do |i|
-        product_weight = product[2][i]
-        product_price = product[3][i]
-        product_image = product[1]
-        product_name = product_name(product[0], product_weight);
+def transform_weight(product_weight_variation)
+  weights = []
 
-        current_product = [product_name, product_price, product_image]
-        write_to_csv(file_name, current_product)
+  product_weight_variation.each_with_index do |weight, index|
+    weights[index] = weight.text.to_s
+  end
 
-        puts "Write #{product_name} in file"
-    end
-end 
-
-def product_name (name, weight)
-    return "#{name} - #{weight}"
+  weights
 end
 
+def transform_price(price_per_weight)
+  prices = []
+
+  price_per_weight.each_with_index do |price, index|
+    prices[index] = price.text.to_s
+  end
+
+  prices
+end
+
+def show_data(product, file_name)
+  (0...product[2].size).each do |i|
+    product_weight = product[2][i]
+    product_price = product[3][i]
+    product_image = product[1]
+    product_name = product_name(product[0], product_weight)
+
+    write_to_csv(file_name, [product_name, product_price, product_image])
+
+    puts "Write #{product_name} in file"
+  end
+end
+
+def product_name(name, weight)
+  "#{name} - #{weight}"
+end
 
 def create_csv(file_name)
-    headers = %w[name price image]
-        CSV.open(file_name, 'w+') do |row|
-        row << headers
-    end
+  headers = %w[name price image]
+  CSV.open(file_name, 'w+') do |row|
+    row << headers
+  end
 end
 
 def write_to_csv(file_name, product)
-    CSV.open(file_name, 'a+') do |row|
-        row << product
-    end
+  CSV.open(file_name, 'a+') do |row|
+    row << product
+  end
 end
 
-parse(url, file_name);
+parse(url, file_name)
